3111d8db01e223f0a6aa2973f55380354616c1a6
ice909 <tonimayloneya@gmail.com>
2025-06-14 17:28:12 +0800

    refactor: move video assets to CDN
    Removed all local video assets from the assets/videos directory and
    updated the baseUrl in global.js to use a CDN location. Videos will
    now be served from https://cdn-nu-common.uniontech.com/deepin-component/
    v25-preview
    instead of being included in the repository. This change will improve
    page load performance and reduce repository size.
    
    refactor: 将视频资源迁移到CDN
    
    从assets/videos目录中删除了所有本地视频资源，并在global.js中更新了
    baseUrl以使用CDN位置。
    视频现在将从https://cdn-nu-common.uniontech.com/deepin-component/v25-
    preview提供服务，
    而不是包含在代码库中。这一更改将提高页面加载性能并减少代码库大小。

--- js/global.js
+++ js/global.js
@@ -2,7 +2,7 @@ const isEn = window.location.pathname.includes('/en');
 const lang = isEn ? 'en' : 'zh';
 
 window.AppConfig = {
-  baseUrl: '',
+  baseUrl: 'https://cdn-nu-common.uniontech.com/deepin-component/v25-preview',
   lang: lang,
 };
 
