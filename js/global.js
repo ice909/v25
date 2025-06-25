a985dceaad1f2608910cbb20d6ec77dab55cc0c9
ice909 <ice909@users.noreply.github.com>
2025-06-25 03:18:28 +0000

    deploy: 37d3acc25ed1549fe1d1ef9d9f63554254d873cf

--- js/global.js
+++ js/global.js
@@ -1,1 +1,1 @@
-const isEn=window.location.pathname.includes("/en");window.AppConfig={oldBaseUrl:"https://cdn-nu-common.uniontech.com/deepin-component/v25-preview",baseUrl:"https://cdn-nu-common.uniontech.com/deepin-component/v25",lang:isEn?"en":"zh"};
+const isEn=window.location.pathname.includes("/en");window.AppConfig={baseUrl:"https://cdn-nu-common.uniontech.com/deepin-component/v25",lang:isEn?"en":"zh"};
